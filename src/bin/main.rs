9b15819edecfe6907e41f6473209810387a12908
spot <spot@spotthecow.com>
2025-12-01 19:30:53 -0800

    try everything to restart wifi

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,7 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use defmt::{debug, error, info, trace, warn};
+use defmt::*;
 use embassy_executor::{Spawner, task};
 use embassy_net::dns::DnsSocket;
 use embassy_net::tcp::client::{TcpClient, TcpClientState};
@@ -16,10 +16,11 @@ use esp_hal::clock::CpuClock;
 use esp_hal::rng::Rng;
 use esp_hal::timer::timg::TimerGroup;
 use esp_radio::Controller;
-use esp_radio::wifi::WifiStaState;
+use esp_radio::wifi::event::{EventExt, StaDisconnected};
+use esp_radio::wifi::{WifiEvent, WifiStaState};
 use esp_radio::{
     // ble::controller::BleConnector,
-    wifi::{ClientConfig, ModeConfig, ScanConfig, WifiController, WifiDevice, WifiEvent},
+    wifi::{ClientConfig, ModeConfig, WifiController, WifiDevice},
 };
 use esp_wmata_pids::wmata::Client;
 use heapless::String;
@@ -100,6 +101,7 @@ async fn main(spawner: Spawner) -> ! {
         .spawn(manage_connection(wifi_controller, ssid, password))
         .unwrap();
     spawner.spawn(net_task(runner)).unwrap();
+    init_wifi_handlers();
 
     while !stack.is_link_up() {
         Timer::after_millis(200).await;
@@ -123,27 +125,28 @@ async fn main(spawner: Spawner) -> ! {
     let mut client = Client::new(reqwless, rx_buf, api_key);
 
     loop {
-        if stack.is_link_up() {
-            let trains = client
-                .next_trains(esp_wmata_pids::wmata::types::Station::K04)
-                .await;
+        // stack may go down but it should come back up eventually
+        while !stack.is_link_up() {
+            Timer::after_millis(1000).await;
+        }
 
-            match trains {
-                Ok(trains) => {
-                    info!("\n\nUpdate: ");
+        let trains = client
+            .next_trains(esp_wmata_pids::wmata::types::Station::K04)
+            .await;
 
-                    let mut format_str: String<48> = String::new();
-                    for t in &trains {
-                        format_str.clear();
-                        t.write_debug_display(&mut format_str)
-                            .expect("couldn't write debug display");
-                        info!("{}", format_str);
-                    }
+        match trains {
+            Ok(trains) => {
+                info!("\n\nUpdate: ");
+
+                let mut format_str: String<48> = String::new();
+                for t in &trains {
+                    format_str.clear();
+                    t.write_debug_display(&mut format_str)
+                        .expect("couldn't write debug display");
+                    info!("{}", format_str);
                 }
-                Err(e) => error!("{:?}", e),
             }
-        } else {
-            warn!("wifi stack was down when trying to fetch. retrying...")
+            Err(e) => error!("{:?}", e),
         }
 
         Timer::after_secs(10).await;
@@ -157,59 +160,85 @@ async fn manage_connection(
     password: &'static str,
 ) {
     debug!("starting manage_connection task");
-    trace!("device capabilities: {:?}", controller.capabilities());
+    debug!("device capabilities: {:?}", controller.capabilities());
+
+    let client_config = ModeConfig::Client(
+        ClientConfig::default()
+            .with_ssid(ssid.into())
+            .with_password(password.into())
+            .with_scan_method(esp_radio::wifi::ScanMethod::AllChannels)
+            .with_failure_retry_cnt(10)
+            .with_channel_none()
+            .with_bssid_none(),
+    );
+
+    controller
+        .set_config(&client_config)
+        .expect("couldn't set wifi controller config");
+
+    // for the loop below, we sleep a short time each iteration.
+    // each iteration where we don't connect succesfully, increase the delay a little bit.
+    // num_iters without connection
+    let mut num_failures = 0u32;
 
     // loop forever, keeping the controller started and the connection up
     loop {
-        let state = esp_radio::wifi::sta_state();
-
-        if state != WifiStaState::Connected {
-            // configure and start the controller if it is not started
-            if !matches!(controller.is_started(), Ok(true)) {
-                let client_config = ModeConfig::Client(
-                    ClientConfig::default()
-                        .with_ssid(ssid.into())
-                        .with_password(password.into()),
-                );
-
+        match esp_radio::wifi::sta_state() {
+            WifiStaState::Started => match controller.connect_async().await {
+                Ok(_) => info!("wifi connected"),
+                Err(e) => {
+                    error!("Failed to connect to wifi: {:?}", e);
+                    num_failures = num_failures.saturating_add(1);
+                }
+            },
+            WifiStaState::Connected => {
+                num_failures = 0;
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            }
+            WifiStaState::Disconnected => {
+                // the amount of trouble in getting a disconnected device to reconnect is not worth it
+                // im still convinced theres a bug in the esp-radio library, where once the controller
+                // fails to connect by reason `Err(Disconnected)` then it is permanently broken until
+                // the controller is restarted. i give up. just restart the whole controller every time
+                warn!("disconnected. restarting the wifi controller");
+                controller.disconnect_async().await.unwrap();
+                controller.stop_async().await.unwrap();
                 controller
                     .set_config(&client_config)
                     .expect("couldn't set wifi controller config");
-
-                controller
-                    .start_async()
-                    .await
-                    .expect("couldn't start wifi controller");
-                info!("wifi controller started");
-
-                // debug!("scanning...");
-                // let scan_config = ScanConfig::default().with_max(10);
-                // let access_points = controller
-                //     .scan_with_config_async(scan_config)
-                //     .await
-                //     .expect("error scanning wifi");
-
-                // for ap in access_points {
-                //     trace!("{:?}", ap);
-                // }
             }
-
-            // controller should be started but not connected here, so let's connect
-            match controller.connect_async().await {
-                Ok(_) => info!("Wifi connected!"),
-                Err(e) => {
-                    error!("Failed to connect to wifi: {:?}", e);
-                    Timer::after_secs(5).await;
-                }
+            WifiStaState::Stopped | WifiStaState::Invalid => match controller.start_async().await {
+                Ok(_) => info!("wifi controller started"),
+                Err(_) => error!("failed to start the wifi controller"),
+            },
+            _ => {
+                error!("Unknown wifi state");
             }
         }
-
-        // controller should be started and connected here, so just sleep before looping
-        Timer::after_secs(5).await;
+        let delay_ms = reconnect_backoff_ms(num_failures);
+        if delay_ms > 0 {
+            warn!("too many connection attempts. sleeping for {}ms", delay_ms);
+            Timer::after_millis(reconnect_backoff_ms(num_failures)).await;
+        }
     }
 }
 
 #[embassy_executor::task]
 async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
     runner.run().await
+}
+
+fn reconnect_backoff_ms(failures: u32) -> u64 {
+    const DELAY_MAX_MS: u64 = 10_000;
+
+    if failures < 3 {
+        return 0;
+    }
+
+    let step = 1u64 << (failures - 3); // 1, 2, 4, 8, ...
+    (500 * step).min(DELAY_MAX_MS)
+}
+
+fn init_wifi_handlers() {
+    StaDisconnected::update_handler(|event| debug!("EVENT: StaDisconnected - {}", event.reason()));
 }
